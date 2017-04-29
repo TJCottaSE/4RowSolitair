adf87d62e8e00a833253d3c5c6905a698915403a
Rogers Michael <rogersmb77@gmail.com>
2017-04-29 07:32:08 -0400

    second commit

--- FourRowSolitaire/src/test/DiscardPileTest.java
+++ FourRowSolitaire/src/test/DiscardPileTest.java
@@ -1,223 +1,152 @@
 package test;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
-import java.awt.Graphics;
 import java.awt.Point;
 
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import FourRowSolitaire.Card;
+import FourRowSolitaire.CardStack;
 import FourRowSolitaire.DiscardPile;
 
 public class DiscardPileTest {
-
-  @BeforeClass
-  public void beforeClass() {
-  }
-
-  @AfterClass
-  public void afterClass() {
-  }
-
-  @Test
-  public void DiscardPile() {
-    throw new RuntimeException("Test not implemented");
-  }
-
-  @Test
-  public void addCard() {
-    throw new RuntimeException("Test not implemented");
-  }
-
-  @Test
-  public void addStack() {
-    throw new RuntimeException("Test not implemented");
-  }
-
-  @Test
-  public void getCardAtLocation() {
-    throw new RuntimeException("Test not implemented");
-  }
-
-  @Test
-  public void getNumViewableCards() {
-    throw new RuntimeException("Test not implemented");
-  }
-
-  @Test
-  public void isValidMoveCard() {
-    throw new RuntimeException("Test not implemented");
-  }
-
-  @Test
-  public void isValidMoveCardStack() {
-    throw new RuntimeException("Test not implemented");
-  }
-
-  @Test
-  public void paint() {
-    throw new RuntimeException("Test not implemented");
-  }
-
-  @Test
-  public void pop() {
-    throw new RuntimeException("Test not implemented");
-  }
-
-  @Test
-  public void pushCard() {
-    throw new RuntimeException("Test not implemented");
-  }
-
-  @Test
-  public void pushCardStack() {
-    throw new RuntimeException("Test not implemented");
-  }
-
-  @Test
-  public void setDrawCount() {
-    throw new RuntimeException("Test not implemented");
-  }
-
-  @Test
-  public void setView() {
-    throw new RuntimeException("Test not implemented");
-  }
-
-  @Test
-  public void undoPop() {
-    throw new RuntimeException("Test not implemented");
-  }
-  
-  @Test
-	public void test_type() throws Exception {
+	 private int drawCount = 1;
+	 private int cardsLeftFromDraw = 0;
+	 
+	 private int cardNumber =1;
+	 private int deckNumber = 1;
+	 private int fullNumber = 2;
+	 DiscardPile discardPile;
+	 Card card;
+	 
+	@BeforeClass
+	public void setUp() throws Exception {
+		discardPile = new DiscardPile(drawCount);
+		card = new Card(Card.HEARTS_SUIT, cardNumber, deckNumber, fullNumber );
 		
-		assertNotNull(DiscardPile.class);
 	}
-  @Test
-	public void test_instantiation() throws Exception {
+
+	@AfterClass
+	public void tearDown() throws Exception {
+		discardPile = null;
+	}
+
+  	/**
+  	 * Test the constructor and setting the draw count
+  	 */
+  	@Test
+ 	public void DiscardPile() {
+   		DiscardPile dp = new DiscardPile(1);
+   		dp.setDrawCount(2);
+   		assertEquals(dp.getDrawCount(), 2);
+  	}
+
+  	@Test
+  	public void testGetNumViewableCards(){
+		// discardPile called push which set number of viewable to zero
+		assertEquals(0, discardPile.getNumViewableCards());
+		//add a card
+		discardPile.addCard(card);
+		assertEquals(1, discardPile.getNumViewableCards());
+		discardPile.addCard(card);
+		assertEquals(2, discardPile.getNumViewableCards());
+  	}
+  	
+  	@Test
+  	public void setView() {
+		discardPile.setView(4);
+		assertEquals(4, discardPile.getNumViewableCards());
+  	}
+  	
+  	@Test
+  	public void addCard() {
+		Card returnedCard;
+		discardPile.addCard(card);
+		returnedCard = discardPile.peek();
+		assertEquals(Card.HEARTS_SUIT, returnedCard.getSuit());
+		assertEquals(2, returnedCard.getFullNumber());
+		assertEquals(1, returnedCard.getNumber());
+  	}
+
+  	@Test
+  	public void addStack() {
+  		DiscardPile dis = new DiscardPile(1);
 		
-		int draw = 0;
-		DiscardPile target = new DiscardPile(draw);
-		assertNotNull(target);
-	}
-  @Test
-	public void test_getNumViewableCards_A$() throws Exception {
+		CardStack cardStack = new CardStack();
+		Card card1 = new Card("Spades", 1, 1, 1);
+		cardStack.addCard(card1);
+		assertEquals(card1, cardStack.pop());
+		cardStack.addCard(card1);
+		dis.addStack(cardStack);
 		
-		int draw = 0;
-		DiscardPile target = new DiscardPile(draw);
-		int actual = target.getNumViewableCards();
-		int expected = 0;
-		assertEquals(expected, actual);
+		Card card2 = dis.pop();
+		assertEquals("Spades", card2.getSuit());
+		//assertEquals(1, dis.pop().getNumber());
+		//assertEquals(1, dis.pop().getFullNumber());
 	}
-  @Test
-	public void test_setView_A$int() throws Exception {
+
+  	@Test
+  	public void pushCard() {
+		int cardNumber = 3;
+		int deckNumber=2;
+		int fullNumber = 10;
+		Card _isPushed;
+		Card cardPush = new Card(Card.DIAMONDS_SUIT, cardNumber, deckNumber, fullNumber );
+		_isPushed = discardPile.push(cardPush);
 		
-		int draw = 0;
-		DiscardPile target = new DiscardPile(draw);
-		int numViewableCards = 0;
-		target.setView(numViewableCards);
-	}
-  @Test
-	public void test_addCard_A$Object() throws Exception {
+		assertEquals(Card.DIAMONDS_SUIT, _isPushed.getSuit());
+		assertEquals(3, _isPushed.getNumber());
+		assertEquals(10, _isPushed.getFullNumber());
 		
-		int draw = 0;
-		DiscardPile target = new DiscardPile(draw);
-		Object card = null;
-		target.addCard(card);
-	}
-  @Test
-	public void test_addStack_A$Object() throws Exception {
+  	}
+  	
+  	@Test
+ 	public void pushCardStack() {
+		DiscardPile dis = new DiscardPile(1);
 		
-		int draw = 0;
-		DiscardPile target = new DiscardPile(draw);
-		Object stack = null;
-		target.addStack(stack);
-	}
-  @Test
-	public void test_push_Object() throws Exception {
+		CardStack cardStack = new CardStack();
+		Card card1 = new Card("Spades", 1, 1, 1);
+		cardStack.addCard(card1);
+		assertEquals(card1, cardStack.pop());
+		cardStack.addCard(card1);
 		
-		int draw = 0;
-		DiscardPile target = new DiscardPile(draw);
-		Object card = null;
-		Object actual = target.push(card);
-		Object expected = null;
-		assertEquals(expected, actual);
-	}
-  @Test
-	public void test_push_A$Object() throws Exception {
-		
-		int draw = 0;
-		DiscardPile target = new DiscardPile(draw);
-		Object stack = null;
-		Object actual = target.push(stack);
-		Object expected = null;
-		assertEquals(expected, actual);
-	}
-  @Test
-	public void test_pop_A$() throws Exception {
-		
-		int draw = 0;
-		DiscardPile target = new DiscardPile(draw);
-		Object actual = target.pop();
-		Object expected = null;
-		assertEquals(expected, actual);
-	}
-  @Test
-	public void test_undoPop_A$() throws Exception {
-		
-		int draw = 0;
-		DiscardPile target = new DiscardPile(draw);
-		Object actual = target.undoPop();
-		Object expected = null;
-		assertEquals(expected, actual);
-	}
-  @Test
-	public void test_getCardAtLocation_A$Point() throws Exception {
-		
-		int draw = 0;
-		DiscardPile target = new DiscardPile(draw);
-		Point p = null;
-		Object actual = target.getCardAtLocation(p);
-		Object expected = null;
-		assertEquals(expected, actual);
-	}
-  @Test
-	public void test_isValidMove_Object() throws Exception {
-		
-		int draw = 0;
-		DiscardPile target = new DiscardPile(draw);
-		Object card = null;
-		boolean actual = target.isValidMove(card);
-		boolean expected = false;
-		assertEquals(expected, actual);
-	}
-  @Test
-	public void test_isValidMove_A$Object() throws Exception {
-		
-		int draw = 0;
-		DiscardPile target = new DiscardPile(draw);
-		Object stack = null;
-		boolean actual = target.isValidMove(stack);
-		boolean expected = false;
-		assertEquals(expected, actual);
-	}
-  @Test
-	public void test_paint_A$Graphics() throws Exception {
-		
-		int draw = 0;
-		DiscardPile target = new DiscardPile(draw);
-		Graphics g = null;
-		target.paint(g);
-	}
+		assertEquals(cardStack, dis.push(cardStack));
+  	}
+  	
+  	@Test
+  	public void pop() {
+  		discardPile.addCard(card);
+		Card poppedCard;
+		poppedCard = discardPile.pop();
+		assertEquals(Card.HEARTS_SUIT, poppedCard.getSuit());
+		assertEquals(2, poppedCard.getFullNumber());
+		assertEquals(1, poppedCard.getNumber());
+  	}
+  	
+  	public void testUndoPop(){
+  		throw new RuntimeException("Test not implemented");
+  	}
+  	
+  	@Test
+	public void testGetCardAtLocationPoint() {
+		Point point = new Point(0,0);
+		discardPile.addCard(card);
+		assertEquals(Card.HEARTS_SUIT, discardPile.getCardAtLocation(point).getSuit());
+  	}
+
+  	@Test
+  	public void isValidMoveCard() {
+  		discardPile.addCard(card);
+		assertTrue(!discardPile.isValidMove(card));
+  	}
+
+  	@Test
+  	public void isValidMoveCardStack() {
+  		assertFalse(discardPile.isValidMove(card));
+  	}
 }
