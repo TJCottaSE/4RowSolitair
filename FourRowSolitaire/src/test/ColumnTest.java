6b3b92ee84d17ba3852c55e569b109aca4251d08
Rogers Michael <rogersmb77@gmail.com>
2017-04-29 20:52:23 -0400

    commit 4

--- FourRowSolitaire/src/test/ColumnTest.java
+++ FourRowSolitaire/src/test/ColumnTest.java
@@ -2,8 +2,14 @@ package test;
 
 import org.testng.TestNG;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
 
+import java.util.ArrayList;
+
+import org.testng.AssertJUnit;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
@@ -15,63 +21,101 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import FourRowSolitaire.Card;
+import FourRowSolitaire.CardStack;
 import FourRowSolitaire.Column;
+import FourRowSolitaire.SingleCell;
 
-public class ColumnTest {
+public class ColumnTest extends CardStack {
+	
+	
+	private final ArrayList<Card> cards = new ArrayList<Card>();
+	
+
+	ColumnTest sc = null;
+	Card card1 = null;
+	Card card2 = null;
+	CardStack stack = new CardStack();
+
+	
+	Card cardPop = stack.pop();
+	Card cardPeek = stack.peek();
+	Card cardPush = sc.push();
  
-  @BeforeClass
-  public void beforeClass() {
-  }
+	@BeforeMethod
+	public void setUp() throws Exception {
+		sc = new ColumnTest();
+		card1 = new Card("Spades", 1, 1, 1);
+		card2 = new Card("Spades", 13, 1, 13);
+	}
 
-  @AfterClass
-  public void afterClass() {
-  }
+	@AfterMethod
+	public void tearDown() throws Exception {
+		sc = null;
+		card1 = null;
+		card2 = null;
+	}
 
   @Test
-  public void Column() {
-    throw new RuntimeException("Test not implemented");
+  public void Column() throws Exception {
+    assertNotNull(Column.class);
+    Column target = new Column();
+	assertNotNull(target);
   }
   
   @Test
-	public void test_type() throws Exception {
-		
-		assertNotNull(Column.class);
-	}
-  @Test
-	public void test_instantiation() throws Exception {
-		
-		Column target = new Column();
-		assertNotNull(target);
-	}
-
+  public void push() throws Exception {
+	  sc.isEmpty();
+	  sc.KING = sc.getNumber(); 
+	  sc.push(card1);
+	  assertEquals(card1, sc.peek());
+	  assertNull(sc.push(card2));
+	
+	  
+		//Column target = new Column();
+		//Object card = null;
+		//Object actual = target.push(card);
+		//Object expected = null;
+		//assertEquals(expected, actual);
+  }
+ 
   @Test
   public void isValidMoveCard() throws Exception {
-		
-		Column target = new Column();
-		Object card = null;
-		boolean actual = target.isValidMove(card);
-		boolean expected = false;
-		assertEquals(expected, actual);
+	  
+	  sc.push(card1);
+	  sc.push(card2);
+	  assertFalse(sc.isValidMove(card1));
+	  sc.isEmpty();
+	  //assertTrue(sc.isValidMove(card1));
+	  
+	  Column target = new Column();
+	  card1 = KING;
+	  boolean actual = target.isValidMove(card1);
+	  boolean expected = false;
+	  assertEquals(expected, actual);
   }
 
   @Test
   public void isValidMoveCardStack() throws Exception {
 		
-		Column target = new Column();
-		Object stack = null;
-		boolean actual = target.isValidMove(stack);
-		boolean expected = false;
-		assertEquals(expected, actual);
-  }
-
-  @Test
-  public void push() throws Exception {
+		CardStack stack = new CardStack();
+		stack.push(card1);
+		stack.push(card2);
+		stack.peek();
+		assertFalse(sc.isValidMove(stack));
 		
-		Column target = new Column();
-		Object card = null;
-		Object actual = target.push(card);
-		Object expected = null;
-		assertEquals(expected, actual);
+		//@Override
+	    //public boolean isValidMove(CardStack stack)
+	    //{
+	        //return isValidMove(stack.peek());
+	    //}
+	
+	  
+		//Column target = new Column();
+		//Object stack = null;
+		//boolean actual = target.isValidMove(stack);
+		//boolean expected = false;
+		//assertEquals(expected, actual);
   }
 
 }
