654b563e18d2c8cd9dbf006f3641f3cb303d705d
Rogers Michael <rogersmb77@gmail.com>
2017-04-30 02:13:10 -0400

    Slight improvement.

--- FourRowSolitaire/src/test/FireworksDisplayTest.java
+++ FourRowSolitaire/src/test/FireworksDisplayTest.java
@@ -2,11 +2,9 @@ package test;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
 
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -15,9 +13,9 @@ import org.testng.annotations.Test;
 import FourRowSolitaire.FireworksDisplay;
 
 public class FireworksDisplayTest {
-
-	FireworksDisplay fw;
 	
+	FireworksDisplay fw;
+
   @BeforeClass
   public void beforeClass() {
 	  fw = new FireworksDisplay(3,2);
@@ -28,14 +26,53 @@ public class FireworksDisplayTest {
   }
 
   @Test
-  public void FireworksDisplay() {
-	  
+  public void FireworksDisplay() throws Exception {
 	  assertEquals(fw.getBackground(), Color.BLACK);
-    
   }
 
   @Test
-  public void randomColor() {
-    assertFalse(fw.randomColor().equals(null));
+  public void randomColor() throws Exception {
+	  
+	  double rand = 0;
+	    if(rand <= .1)
+        {
+	    	assertFalse(fw.randomColor().equals(null));
+        }
+        if(rand <= .2)
+        {
+        	assertFalse(fw.randomColor().equals(null));
+        }
+        if(rand <= .3)
+        {
+        	assertFalse(fw.randomColor().equals(null));
+        }
+        if(rand <= .4)
+        {
+        	assertFalse(fw.randomColor().equals(null));
+        }
+        if(rand <= .5)
+        {
+        	assertFalse(fw.randomColor().equals(null));
+        }
+        if(rand <= .6)
+        {
+        	assertFalse(fw.randomColor().equals(null));
+        }
+        if(rand <= .7)
+        {
+        	assertFalse(fw.randomColor().equals(null));
+        }
+        if(rand <= .8)
+        {
+        	assertFalse(fw.randomColor().equals(null));
+        }
+        if(rand <= .9)
+        {
+        	assertFalse(fw.randomColor().equals(null));
+        }
+
+	  assertFalse(fw.randomColor().equals(null));
+	  
   }
+
 }
